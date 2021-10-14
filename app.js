2d4c2ded7ed2090d814937c2b7cf3bb6a655085f
MightyBenno <88509973+benyvue@users.noreply.github.com>
2021-10-13 22:35:23 -0500

    refactored template literal codes and code interpolated/destructured to pull in appp.js info to generate index page

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const inquirer = require('inquirer');
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//   if (err) throw err;
-
-//   console.log('Portfolio complete! Check out index.html to see the output!')
-// });
+// const mockData = {
+//   name: 'Lernantino',
+//   github: 'lernantino',
+//   projects: []
+// }
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -139,8 +137,18 @@ Add a New Project
   });
 };
 
+// const pageHTML = generatePage(mockData);
+
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+   const pageHTML = generatePage(portfolioData);
+
+
+   fs.writeFile('./index.html', pageHTML, err => {
+     if (err) throw err;
+
+     console.log('Portfolio complete! Check out index.html to see the output!')
+   });
+
   });
